0815e007b562dd90f2fe1bd34889f94db789cfa5
1561757399@qq.com <1561757399@qq.com>
2020-10-14 09:38:50 +0800

    add the resources dir and add the test.txt file

--- src/show.py
+++ src/show.py
@@ -1,15 +1,27 @@
 import calendar
 import time
 
-cal=calendar.month(2020,12)
+from pip._vendor.distlib.compat import raw_input
+
+from src import test
+cal = calendar.month(2020, 12)
 print("以下输出为2020年12月日历：")
 print(cal)
 
-"""time=time.clock()"""
+# time = time.clock()
 
-print(time)
+# print(time)
 
-cal_name=dir(calendar)
-print("CALENDAR_NAME:",cal_name)
-time_name=dir(time)
-print("TIME_NAME:",time_name)
+test.show()
+cal_name = dir(calendar)
+print("CALENDAR_NAME:", cal_name)
+time_name = dir(time)
+print("TIME_NAME:", time_name)
+
+str_1 = raw_input("请输入:\n")
+
+print(str_1)
+
+str_2 = input("请输入：\n")
+
+print(str_2)
