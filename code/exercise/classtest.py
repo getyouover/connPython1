004c89441ddd26d89cb50dc7600f64ba2a111e36
1561757399@qq.com <1561757399@qq.com>
2020-10-21 15:39:44 +0800

    python 学习练习实例 day 4---面向对象 类 的使用

---
+++ code/exercise/classtest.py
@@ -0,0 +1,32 @@
+class TVShow:
+    def __init__(self, show):
+        self.__show = show
+
+    @property
+    def show(self):
+        return self.__show
+
+class Fruit:
+    color = None
+
+    def __init__(self, color="绿色"):
+        Fruit.color = color
+
+    def hverst(self):
+        print("水果是：" + Fruit.color + "的！")
+
+
+class Apple(Fruit):
+    def __init__(self):
+        super().__init__()
+        print("I am an Apple")
+
+
+if __name__ == "__main__":
+    TV = TVShow("齐天大圣")
+    print("播放中："+TV.show)
+    #print("播放中：" + TV.__show)
+    print("播放中：" + TV._TVShow__show)
+    print("播放中：" + str(TVShow.show))
+    apple = Apple()
+    apple.hverst()
