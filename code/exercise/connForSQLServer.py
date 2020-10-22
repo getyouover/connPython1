2c122b65d488a0d02bbced4a73667ca574001339
1561757399@qq.com <1561757399@qq.com>
2020-10-22 10:53:07 +0800

    python 学习练习实例 day 5---test for connection SQL Server DB 数据乱码

--- code/exercise/connForSQLServer.py
+++ code/exercise/connForSQLServer.py
@@ -2,15 +2,47 @@ import pymssql
 
 
 class MSSQL:
-    host=""
-    username=""
-    passwrd=""
-    dbname=""
-    encoding="UTF-8"
+    host = ""
+    username = ""
+    passwrd = ""
+    dbname = ""
+    encoding = "UTF-8"
 
-    def __init__(self, host,username,passwrd,dbname,encoding):
-        self.host=host
-        self.username=username
-        self.passwrd=passwrd
-        self.dbname=dbname
-        self.encoding=encoding
+    def __init__(self, host,username,passwrd,dbname):
+        self.host = host
+        self.username = username
+        self.passwrd = passwrd
+        self.dbname = dbname
+
+        print("数据库连接初始化")
+
+    def __GetConnnect(self):
+        self.conn = pymssql.connect(self.host, self.username, self.passwrd, self.dbname, "UTF-8")
+        cur = self.conn.cursor()
+        if not cur:
+            raise(NameError, "数据库连接获取失败！")
+        else:
+            return cur
+
+    def ExecQuery(self, sql):
+        cur = self.__GetConnnect()
+        cur.execute(sql)
+        result = cur.fetchall()
+
+        self.conn.close()
+        return result
+
+    def ExecNoQuery(self, sql):
+        cur = self.__GetConnnect()
+        cur.execute(sql)
+        self.conn.commiy()
+        self.conn.close()
+
+def main():
+        m = MSSQL(host="127.0.0.1", username="robin", passwrd="test123123", dbname="pythontest")
+        result_1 = m.ExecQuery("select id, number,convert(nvarchar(50),S_name) as S_name,S_age from students")
+        print(result_1)
+
+
+if __name__ == "__main__":
+    main()
